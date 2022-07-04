0d8fa8005001ed66d9bcd42340ebc3d1fbf413c2
SouthCraftX <96909633+SouthCraftX@users.noreply.github.com>
2022-07-04 10:46:23 +0800

    Add files via upload

--- minizip_test.c
+++ minizip_test.c
@@ -3,11 +3,25 @@
 #include <minizip\zip.h>
 #include <minizip\unzip.h>
 
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+
 #define  MAX_PATH_LEN (260)
-#define PATH_TO_UNZIP (".\\TEMP\\noteColor.png")
+#define PATH_TO_UNZIP (".\\SC3C_TEMP\\noteColor.png")
 typedef unsigned char byte;
 
-inline strcmp_ignore_case( const char* str_x , const char* str_y ){
+size_t writepng( void* buffer , size_t nwrite ,FILE* stream  ){
+#ifdef USE_UNLOCKED_FWRITE
+    return _fwrite_nolock( buffer , nwrite , 1 ,stream );
+#else
+    return fwrite( buffer , nwrite , 1 ,stream );
+#endif
+}
+
+int strcmp_ignore_case( const char* str_x , const char* str_y ){
 #ifdef _WIN32
     return stricmp( str_x , str_y );
 #else
@@ -15,8 +29,15 @@ inline strcmp_ignore_case( const char* str_x , const char* str_y ){
 #endif
 }
 
+void clean_cache(){
+    if(access( PATH_TO_UNZIP , 0 )==F_OK){
+        if(remove( PATH_TO_UNZIP ))
+            QUITMSG("Failed to clean cache.");
+    }
+}
+
 //Print a error message and exit.
-inline QUITMSG( const char* msg ){
+inline void QUITMSG( const char* msg ){
     fprintf( stderr , msg );
     exit();
 }
@@ -33,6 +54,7 @@ int main(){
 
     unzFile zipfile =  unzOpen64( "MidiVoyager.zip");
 
+    clean_cache();
     if( zipfile == NULL ) 
         QUITMSG("Failed to open the zip.");
 
@@ -55,7 +77,7 @@ int main(){
             QUITMSG(" PNG file is too large.");
 
         byte* io_buffer = NULL;
-        io_buffer = (byte*)malloc(  );
+        io_buffer = (byte*)malloc( file_info.uncompressed_size );
         if( io_buffer == NULL )
             QUITMSG( "Failed to allocate memory for unziping.");
         
@@ -64,6 +86,13 @@ int main(){
         if( read_n < file_info.uncompressed_size)
             QUITMSG(" Error size.");
 
+        FILE* write_png_fptr = fopen( PATH_TO_UNZIP , "wb" );
+        if( write_png_fptr == NULL)
+            QUITMSG( "Failed to create temporary file." );
+
+        if( !writepng( io_buffer , file_info.uncompressed_size , write_png_fptr ))
+            QUITMSG( "Failed to write temporary file." );
+    
 
         break;
         GOTOLAB_NEXTFILE:
